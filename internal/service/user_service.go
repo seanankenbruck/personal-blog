be1bb3d5e305abfdbb395068a2ee6877fe466b16
smankenb <sean.ankenbruck@gmail.com>
2025-05-13 10:11:45 -0400

    add password validation

--- internal/service/user_service.go
+++ internal/service/user_service.go
@@ -2,10 +2,14 @@ package service
 
 import (
 	"context"
+	"errors"
 	"golang.org/x/crypto/bcrypt"
+	"regexp"
 
 	"github.com/seanankenbruck/blog/internal/domain"
 )
+
+var passwordRegex = regexp.MustCompile(`[!@#\$%\^&\*\(\)_\+\-=\[\]{};':"\\|,.<>\/?]+`)
 
 // userService implements the UserService interface
 type userService struct {
@@ -19,12 +23,19 @@ func NewUserService(repo domain.UserRepository) domain.UserService {
 
 // CreateUser creates a new user
 func (s *userService) CreateUser(ctx context.Context, user *domain.User) error {
+	// Validate password
+	if len(user.Password) < 8 {
+		return errors.New("password must be at least 8 characters long")
+	}
+	if !passwordRegex.MatchString(user.Password) {
+		return errors.New("password must include at least one special character")
+	}
 	// Hash the password before storing
 	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
 	if err != nil {
 		return err
 	}
-	user.Password = string(hashedPassword)
+	user.Password = string(hashedPassword) // Store as bcrypt hash
 	return s.repo.Create(ctx, user)
 }
 
